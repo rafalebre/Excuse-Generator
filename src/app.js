23d9b377ea481188c9ee0d83180fd3db9102fc73
rafalebre <rafaellebre100@gmail.com>
2023-01-03 19:40:47 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function onload() {
+  document.querySelector("#excuse").innerHTML = excusegenerator();
+};
+
+let excusegenerator = function onload() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoidx = Math.floor(Math.random() * who.length);
+  let actionidx = Math.floor(Math.random() * action.length);
+  let whatidx = Math.floor(Math.random() * what.length);
+  let whenidx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoidx] +
+    " " +
+    action[actionidx] +
+    " " +
+    what[whatidx] +
+    " " +
+    when[whenidx]
+  );
 };
